17702a05b32f4f732d68fd82c270b79fc7d1be21
PRINCEZ110 <deeplaxme@gmail.com>
2026-05-19 15:41:00 +0545

    db connection added

--- src/main/java/com/sahakarinet/sahakarinet/SahakariNet/Utills/DbConnection.java
+++ src/main/java/com/sahakarinet/sahakarinet/SahakariNet/Utills/DbConnection.java
@@ -1,4 +1,25 @@
-package com.sahakarinet.sahakarinet.SahakariNet.Utills;
+package com.sahakarinet.sahakari.SahakariNet.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DbConnection {
-}
+    // link for database connection
+    private static String url = "jdbc:mysql://localhost:3306/sahakarinet";
+    // username
+    private static String user = "root";
+    private static String pass = "1234";
+
+    // creates database connection and return
+    // uses throws SQLException for handle error or connection fails
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        Connection conec = DriverManager.getConnection(url, user, pass);
+        return conec;
+    }
