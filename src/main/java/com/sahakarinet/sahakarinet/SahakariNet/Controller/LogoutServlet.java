6b05bedeb812b996d2abf9961c9f9b5cdd15af52
Dibya <www.dibyabantawa666@gmail.com>
2026-05-20 18:51:16 +0545

    Add ContactServlet, LogoutServlet, ForgotPasswordServlet from Dibya

--- src/main/java/com/sahakarinet/sahakarinet/SahakariNet/Controller/LogoutServlet.java
+++ src/main/java/com/sahakarinet/sahakarinet/SahakariNet/Controller/LogoutServlet.java
@@ -1,4 +1,19 @@
 package com.sahakarinet.sahakarinet.SahakariNet.Controller;
 
-public class LogoutServlet {
+import java.io.IOException;
+
+import com.sahakarinet.sahakarinet.SahakariNet.Utills.SessionUtil;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        SessionUtil.clearSession(req);
+        res.sendRedirect(req.getContextPath() + "/login.jsp?logout=true");
+    }
 }
